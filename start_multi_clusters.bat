14e015b6a25939103d6497ae86dd13d8413128a8
Moritz Wassmer <moritz@wassmer-bw.de>
2025-01-28 16:22:33 +0100

    parallel execution with kind working but workloads stuck in pending

--- start_multi_clusters.bat
+++ start_multi_clusters.bat
@@ -5,8 +5,8 @@ start kind.exe
 :: cleanup then create clusters
 kind delete cluster --name cluster-carbonaware
 kind delete cluster --name cluster-normal
-kind create cluster --config=C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\3_nodes_cluster_config.yml --name cluster-carbonaware
-kind create cluster --config=C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\3_nodes_cluster_config2.yml --name cluster-normal
+kind create cluster --config=C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\cconf_carbonaware.yml --name cluster-carbonaware
+kind create cluster --config=C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\cconf_normal.yml --name cluster-normal
 
 cd C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4
 
@@ -18,4 +18,11 @@ kubectl apply -f C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\deploy
 :: Normal execution
 kubectl config use-context kind-cluster-normal
 kubectl apply -f C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\rbac.yaml
-kubectl apply -f C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\deployment.yml
+kubectl apply -f C:\Users\morit\OneDrive\UNI\Master\WS24\CC\Assignments\4\deployment_normal.yml
+
+:: only when necessary
+
+kubectl cp :/app/normal_strategy.log results/normal_strategy.log
+kubectl config use-context kind-cluster-carbonaware
+kubectl cp :/app/carbonaware_strategy.log results/carbonaware_strategy.log
+kubectl rollout restart deployment kopfexample-operator
